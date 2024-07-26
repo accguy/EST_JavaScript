0ea56afbe5121859bdd54bf33d13ce4d6a577a8b
accguy <accguy2190@naver.com>
2024-07-26 16:33:56 +0900

    7월 26일 금요일

--- todo/index.js
+++ todo/index.js
@@ -3,27 +3,31 @@ const $button = document.querySelector("button");
 const $ul = document.querySelector("ul");
 const $form = document.querySelector("form");
 
+const createUi = (todoData) => {
+  const $li = document.createElement("li");
+  const $newBtn = document.createElement("button");
+  $newBtn.innerText = "삭제";
+  $newBtn.classList.add(`id_${todoData.id}`);
+
+  $newBtn.addEventListener("click", function (e) {
+    deleteTodo(e.target.classList[0].split("_")[1]);
+  });
+
+  $li.appendChild(document.createTextNode(todoData.todo));
+  $li.appendChild($newBtn);
+  $ul.appendChild($li);
+
+  $li.style.marginBottom = "10px";
+  $newBtn.style.marginLeft = "10px";
+};
+
 // fetchTodos
 const fetchTodos = async function () {
   const res = await fetch("http://localhost:3000/todos");
   const todos = await res.json();
 
   todos.forEach((item) => {
-    const $li = document.createElement("li");
-    const $newBtn = document.createElement("button");
-    $newBtn.innerText = "삭제";
-    $newBtn.classList.add(`id_${item.id}`);
-
-    $newBtn.addEventListener("click", function (e) {
-      deleteTodo(e.target.classList[0].split("_")[1]);
-    });
-
-    $li.appendChild(document.createTextNode(item.todo));
-    $li.appendChild($newBtn);
-    $ul.appendChild($li);
-
-    $li.style.marginBottom = "10px";
-    $newBtn.style.marginLeft = "10px";
+    createUi(item);
   });
 };
 // 추가 함수
